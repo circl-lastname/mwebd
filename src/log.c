ddb649c432bffc6b5a18ee4a3259e68e0f994386
circl <circl.lastname@gmail.com>
2023-11-13 18:31:43 +0100

    Add logging facilities

---
+++ src/log.c
@@ -0,0 +1,42 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void log_info(char* format, ...) {
+  va_list arguments;
+  va_start(arguments, format);
+  
+  vfprintf(stderr, format, arguments);
+  
+  va_end(arguments);
+}
+
+void log_warn(char* format, ...) {
+  va_list arguments;
+  va_start(arguments, format);
+  
+  if (isatty(STDERR_FILENO)) {
+    fputs("\x1b[33m", stderr);
+    vfprintf(stderr, format, arguments);
+    fputs("\x1b[0m", stderr);
+  } else {
+    vfprintf(stderr, format, arguments);
+  }
+  
+  va_end(arguments);
+}
+
+void log_crit(char* format, ...) {
+  va_list arguments;
+  va_start(arguments, format);
+  
+  if (isatty(STDERR_FILENO)) {
+    fputs("\x1b[31m", stderr);
+    vfprintf(stderr, format, arguments);
+    fputs("\x1b[0m", stderr);
+  } else {
+    vfprintf(stderr, format, arguments);
+  }
+  
+  va_end(arguments);
+}
