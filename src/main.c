645a5f757e279f79279f3a49abc1066b665456d7
circl <circl.lastname@gmail.com>
2023-11-18 12:40:08 +0100

    Add basic main

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <mwebd/config.h>
+#include <mwebd/log.h>
+
+int main(int argc, char** argv) {
+  log_info("MWebD - Modular HTTP server and backend runtime\n");
+  
+  if (argc != 2) {
+    log_crit("Invalid amount of arguments, usage: mwebd <config_file>\n");
+    return 1;
+  }
+  
+  config_init(argv[1]);
+  
+  return 0;
+}
