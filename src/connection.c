38bc3849a18813234dec2ec0b0da072aff2529c9
circl <circl.lastname@gmail.com>
2023-11-29 17:40:37 +0100

    Read incoming request

--- src/connection.c
+++ src/connection.c
@@ -3,8 +3,66 @@
 
 #include "connection.h"
 
+_Thread_local static int connection;
+_Thread_local static char request_buf[16384];
+_Thread_local static size_t request_size;
+
+static void read_request() {
+  char ch;
+  request_size = 0;
+  
+  unsigned state = 0;
+  
+  while (true) {
+    if (request_size == 16384) {
+      // TODO: actually respond
+      close(connection);
+      pthread_exit(NULL);
+    }
+    
+    if (read(connection, &ch, 1) != 1) {
+      close(connection);
+      pthread_exit(NULL);
+    }
+    
+    request_buf[request_size] = ch;
+    request_size++;
+    
+    switch (state) {
+      case 0:
+        if (ch == '\r') {
+          state++;
+        }
+      break;
+      case 1:
+        if (ch == '\n') {
+          state++;
+        } else {
+          state = 0;
+        }
+      break;
+      case 2:
+        if (ch == '\r') {
+          state++;
+        } else {
+          state = 0;
+        }
+      break;
+      case 3:
+        if (ch == '\n') {
+          return;
+        } else {
+          state = 0;
+        }
+      break;
+    }
+  }
+}
+
 void* connection_main(void* connection_raw) {
-  int connection = (long)connection_raw;
+  connection = (long)connection_raw;
+  
+  read_request();
   
   close(connection);
   pthread_exit(NULL);
