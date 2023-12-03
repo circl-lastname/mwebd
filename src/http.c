51c2ea5630c7225ef81ce0f2edbfe7c8c7bfee3d
circl <circl.lastname@gmail.com>
2023-12-03 12:48:40 +0100

    Revert "Make i global in HTTP parser"
    This reverts commit e3c38dca3c682df0a4b68a698a8c3cb9efd9fb1c.

--- src/http.c
+++ src/http.c
@@ -5,40 +5,38 @@
 
 #include "default_responses.h"
 
-_Thread_local static unsigned request_i;
-
 status_t http_parse(char* request_buf, size_t request_size, method_t* method, char** uri, hashmap_t** hashmap) {
-  request_i = 0;
+  unsigned i = 0;
   
   if (!memcmp(request_buf, "GET ", 4)) {
     *method = METHOD_GET;
-    request_i += 4;
+    i += 4;
   } else if (!memcmp(request_buf, "HEAD ", 5)) {
     *method = METHOD_HEAD;
-    request_i += 5;
+    i += 5;
   } else if (!memcmp(request_buf, "POST ", 5)) {
     *method = METHOD_POST;
-    request_i += 5;
+    i += 5;
   } else if (!memcmp(request_buf, "PUT ", 4)) {
     *method = METHOD_PUT;
-    request_i += 4;
+    i += 4;
   } else if (!memcmp(request_buf, "DELETE ", 7)) {
     *method = METHOD_DELETE;
-    request_i += 7;
+    i += 7;
   } else if (!memcmp(request_buf, "CONNECT ", 8)) {
     *method = METHOD_CONNECT;
-    request_i += 8;
+    i += 8;
   } else if (!memcmp(request_buf, "OPTIONS ", 8)) {
     *method = METHOD_OPTIONS;
-    request_i += 8;
+    i += 8;
   } else if (!memcmp(request_buf, "TRACE ", 6)) {
     *method = METHOD_TRACE;
-    request_i += 6;
+    i += 6;
   } else {
     return STATUS_501;
   }
   
-  if (request_i >= request_size) {
+  if (i >= request_size) {
     return STATUS_400;
   }
   
